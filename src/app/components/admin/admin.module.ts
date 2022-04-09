f7e294f30ad0ed41eda5883ccebbd21600258b7c
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-04-09 14:31:55 -0300

    Fix - configurações de roteamento e lazy loading

---
+++ src/app/components/admin/admin.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//routing
+import { AdminRoutingModule } from './admin-routing.module';
+
+//pages
+import { HomeComponent } from './../../../../../../../routes/src/app/pages/home/home.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
